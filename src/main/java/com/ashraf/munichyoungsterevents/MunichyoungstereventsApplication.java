d08242103edfa1ef5bdefa1d6b1f0c471dee1550
Wizard-of-Zamorak <benaliachraf1994@gmail.com>
2026-07-11 20:56:23 +0200

    Test git access

--- src/main/java/com/ashraf/munichyoungsterevents/MunichyoungstereventsApplication.java
+++ src/main/java/com/ashraf/munichyoungsterevents/MunichyoungstereventsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+//test git comment
 public class MunichyoungstereventsApplication {
 
 	public static void main(String[] args) {
